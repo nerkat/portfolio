7dd8220187144632f25b0b4b294e33471b7b9669
nerkat <nerkat@gmail.com>
2023-01-07 20:05:06 +0700

    back iamge working

--- webpack.config.js
+++ webpack.config.js
@@ -34,9 +34,7 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader',
-                ],
+                type: 'asset/resource'
             },
         ],
     },
