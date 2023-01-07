a95cecd149fc16c6964527b90f2f6fbce1b8e06c
nerkat <nerkat@gmail.com>
2023-01-07 21:08:07 +0700

    draft v1!!

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
                 ],
             },
             {
-                test: /\.(png|svg|jpg|gif)$/,
+                test: /\.(png|svg|jpg|gif|ttf)$/,
                 type: 'asset/resource'
             },
         ],
