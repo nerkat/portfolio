2ab6f4aebe2f1055a986262799d4b7b0f822834a
nerkat <nerkat@gmail.com>
2023-01-27 03:08:54 +0700

    active nav link

--- src/script/slider.js
+++ src/script/slider.js
@@ -7,17 +7,30 @@ const certificatesLink = document.querySelector("#nav-link-certificates");
 
 
 aboutLink.addEventListener("click", () => {
+  changeActiveLink(aboutLink);
   slidesContainer.scrollLeft = 0;
 });
 
 resumeLink.addEventListener("click", () => {
+  changeActiveLink(resumeLink);
   slidesContainer.scrollLeft = slide.clientWidth;
 });
 
 galleryLink.addEventListener("click", () => {
+  changeActiveLink(galleryLink);
   slidesContainer.scrollLeft = slide.clientWidth * 2;
 });
 
 certificatesLink.addEventListener("click", () => {
+  changeActiveLink(certificatesLink);
   slidesContainer.scrollLeft = slide.clientWidth * 3;
 });
+
+changeActiveLink = (activeLink) => {
+  const links = document.querySelectorAll(".nav-item");
+  links.forEach((link) => {
+    link.classList.remove("active");
+  });
+  activeLink.classList.add("active");
+
+};
